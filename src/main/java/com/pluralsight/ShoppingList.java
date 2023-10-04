3e385bddebc654573e415471841f879f957accda
Christian Crites <ccrites98@gmail.com>
2023-10-04 11:45:30 -0400

    sl sl sl with '#' will be ignored, and an empty message aborts the commit.

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+System.out.println("Flour, Sugar, Toothpaste, Eggs, Oranges, Grapes, Apples, Pears, Cinnamon, AllSpice, Cloves");
+
+
+    }
+}
